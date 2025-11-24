4a8d6d0461116afec580c9f868cc5fe50af80e1a
Paul Gratacos <pgrataco@f6r3s6.clusters.42paris.fr>
2025-11-24 16:41:47 +0100

    ver 0.2

--- loop.cpp
+++ loop.cpp
@@ -1,67 +1,106 @@
 #include "structure.hpp"
 
-Structure::Structure()
-{
-    my_sockaddr.sin_family = AF_INET;
-    my_sockaddr.sin_addr.s_addr = INADDR_ANY;
-    my_sockaddr.sin_port = htons(8080);
-
-    my_pollfd.fd = -1;
-    my_pollfd.events = POLLIN;
-    my_pollfd.revents = -1;
-}
-
 void main_loop()
 {
+    /*
+    TODO ->parse config and get a struct with all the info
+    */
+    std::vector <int> ports = {8080, 8002};
+
     Structure st;
+    int vsize = ports.size();
+    int portsparser = 0;
+    while (portsparser< vsize)
+    {
+        st.addListener(ports, portsparser);
+        portsparser++;
+    }
+
+    while (true)
+    {
+        std::vector <struct pollfd> pollFdArray = makeArray(st);
+        int n = poll(pollFdArray.data(), pollFdArray.size(), -1);
+        if (n < 0)
+        {
+            //FAIL STATE
+        }
+        for(size_t i = 0; i < pollFdArray.size(); i++)
+        {
+            st.getEntry(i).setRevent(pollFdArray[i].revents);
+            if (pollFdArray[i].revents == 0)
+                continue;
+            if (st.getEntry(i).getIsListener() && (pollFdArray[i].revents & POLLIN))
+            {
+                struct sockaddr_in client_addr;
+                socklen_t addrlen = sizeof(client_addr);
+                int client_fd = accept(pollFdArray[i].fd, (struct sockaddr *)&client_addr, &addrlen);
+                if (client_fd < 0)
+                {
+                    //FAIL STATE
+                }else{
+                    st.addClient(client_fd);
+                    const char* response =
+                        "HTTP/1.1 200 OK\r\n"
+                        "Content-Type: text/plain\r\n"
+                        "Content-Length: 12\r\n"
+                        "\r\n"
+                        "Hello world";
+
+                        send(client_fd, response, std::strlen(response), 0);
+                }
+            }   
+        }
+    }
+
+}
+
+void Structure::addListener(std::vector <int> ports, int portsparser)
+{
+    struct sockaddr_in sockaddr;
+
+    memset(&sockaddr, 0, sizeof(sockaddr));
+    sockaddr.sin_family = AF_INET;
+    sockaddr.sin_addr.s_addr = INADDR_ANY;
+    sockaddr.sin_port = htons(ports[portsparser]);
+
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-
     fcntl(sock, F_SETFL, O_NONBLOCK);
-
-    st.setFd(sock);
-
-    bind(sock, st.getSockAddr(), st.getSize());
-
+    bind(sock, (const struct sockaddr *)&(sockaddr), sizeof(sockaddr));
     listen(sock, SOMAXCONN);
 
-    while (true) {
-        int client_fd = accept(sock, NULL, NULL);
+    struct pollfd pfd;
+    pfd.fd = sock;
+    pfd.events = POLLIN;
+    pfd.revents = 0;
 
-        const char* response =
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: text/plain\r\n"
-            "Content-Length: 12\r\n"
-            "\r\n"
-            "Hello world";
+    this->pollEntries.push_back(PollEntry(pfd, true));
+}
 
-        send(client_fd, response, std::strlen(response), 0);
-        close(client_fd);
+void Structure::addClient(int client_fd)
+{
+    struct pollfd client_pfd;
+    client_pfd.fd = client_fd;
+    client_pfd.events = POLLIN;
+    client_pfd.revents = 0;
+    this->pollEntries.push_back(PollEntry(client_pfd, false));
+}
+
+std::vector <struct pollfd> makeArray(Structure st)
+{
+    std::vector <struct pollfd> ret;
+    for(size_t i = 0; i < st.getEntries().size(); i++)
+    {
+        ret.push_back(st.getEntry(i).getPollfd());
     }
-    close(sock);
-    //poll(st.getMypollfd(), st.getEvent(), -1);
+    return (ret);
 }
 
-const sockaddr* Structure::getSockAddr()
+std::vector <PollEntry> Structure::getEntries()
 {
-    return ((const struct sockaddr *)&(this->my_sockaddr));
+    return (this->pollEntries);
 }
 
-socklen_t Structure::getSize() 
+PollEntry Structure::getEntry(int index)
 {
-    return (sizeof(my_sockaddr));
-}
-
-void Structure::setFd(int sock)
-{
-    this->my_pollfd.fd = sock;
-}
-
-pollfd* Structure::getMypollfd()  
-{
-    return (&(this->my_pollfd));
-}
-
-nfds_t Structure::getEvent() 
-{
-    return (this->my_pollfd.events);
+    return (this->pollEntries[index]);
 }
--- structure.hpp
+++ structure.hpp
@@ -5,30 +5,36 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <fcntl.h>
-#include <poll.h>
 #include <cstring>
 #include <unistd.h>
+#include <vector>
+#include "pollEntry.hpp"
 
 
 class Structure
 {
     private:
-        struct sockaddr_in my_sockaddr;
-        struct pollfd my_pollfd;
-        socklen_t addrlen;
+        std::vector<PollEntry> pollEntries;
     public:
-        Structure();
+        Structure(){};
         ~Structure(){};
 
+        void addListener(std::vector <int> ports, int portsparser);
+        void addClient(int client_fd);
+
         pollfd* getMypollfd();
+
+        std::vector <PollEntry> getEntries();
+        PollEntry getEntry(int index);
+
+        void setRevent(short revent);
 
         const sockaddr* getSockAddr();
         socklen_t getSize();
         nfds_t getEvent();
-        void setFd(int sock);
 };
 
 void main_loop();
-
+std::vector <struct pollfd> makeArray(Structure st);
 
 #endif
